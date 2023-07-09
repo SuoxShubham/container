66ac3262625585ef7c1da5387fa2652b1852658f
Shubham <shubham.srivastava@macys.com>
2023-07-09 20:08:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./index.scss";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div>Container Page</div>
+    <Footer/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer(){
+return(
+    <div className="p-5 bg-red-500 text-white text-3xl font-bold"> This is landing page footer</div>
+)
+}
